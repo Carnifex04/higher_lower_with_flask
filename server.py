ed5f8ea96c1c6bf39d3eb016c86a478360c0607d
hriday <guptahriday4444@gmail.com>
2022-09-28 13:21:17 +0530

    added home

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+answer = random.randint(0, 9)
+
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return "<h1> Guess a number between 0 and 9 </h1>" \
+           "<img src = 'https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
